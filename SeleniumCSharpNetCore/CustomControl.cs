27f19e35bb21ed56eee53f19afe4fcd85cbe4951
Vedad Hadžihasanović <vedad.hadzihasanovic@gmail.com>
2021-01-13 15:18:07 +0100

    Initial Commit until combo control
    Initial Commit until combo control

---
+++ SeleniumCSharpNetCore/CustomControl.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCSharpNetCore
+{
+    public class CustomControl : DriverHelper //This way you extend Driver
+    {
+        public static void ComboBox(string controlName, string value)
+        {
+
+            IWebElement comboControl = Driver.FindElement(By.XPath($"//input[@id='{controlName}-awed']"));
+
+            comboControl.Clear();
+            comboControl.SendKeys("Almond");
+
+            IWebElement comboBox = Driver.FindElement(By.XPath($"//div[@id='{controlName}-dropmenu']//li[text()='{value}']"));
+            comboBox.Click();
+        }
+    }
+}
---
+++ SeleniumCSharpNetCore/UnitTest1.cs
@@ -0,0 +1,39 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumCSharpNetCore
+{
+    public class Tests : DriverHelper //This way you extend Driver
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+            System.Console.WriteLine("Setup");
+            Driver = new ChromeDriver() ;   
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Driver.Navigate().GoToUrl("https://demowf.aspnetawesome.com/");
+            System.Console.WriteLine("Setup");
+
+            Driver.FindElement(By.Id("ContentPlaceHolder1_Meal")).SendKeys("Tomato");
+            //Narednom komandom trazimo element sa custom xpath selectorom i klikamo na zeljeno dugme
+            Driver.FindElement(By.XPath("//input[@Name='itemContentPlaceHolder1_ParentCat']/following-sibling::div[text()='Nuts']")).Click();
+            //Narednom komandom trazimo element sa custom xpath selectorom i klikamo na zeljeno dugme
+            Driver.FindElement(By.XPath("//input[@Name='ctl00$ContentPlaceHolder1$ChildMeal1']/following-sibling::div[text()='Hazelnuts']")).Click();
+
+
+            //The first command needs to be implamented before second one, because second element is only visible after first element value is set.
+            //We Debug this program by entering red dots at point where commands need to be executed to track they behavior.
+            //We can drag yellow arrow from red dot to some other line and by pressing 'Step over' see changes without compiling code again.
+
+            CustomControl.ComboBox("ContentPlaceHolder1_AllMealsCombo", "Almond");
+
+            System.Console.WriteLine("Test1");
+        }
+    }
+}
